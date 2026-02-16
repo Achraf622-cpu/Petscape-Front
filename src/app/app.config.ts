d2502354dd18ece2e219944f56d0948688d1406e
Achraf622-cpu <hanzazachraf581@gmail.com>
2026-02-16 01:52:16 +0000

    PET-59: Setup Angular application providers

---
+++ src/app/app.config.ts
@@ -0,0 +1,17 @@
+import { ApplicationConfig, isDevMode } from '@angular/core';
+import { provideRouter, withViewTransitions, withComponentInputBinding } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { routes } from './app.routes';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes, withViewTransitions(), withComponentInputBinding()),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
+};
